2fe740dc4b9e04326fa479454c9c0d7bf70fe643
cristian <cristian.rojas@live.fr>
2026-06-22 16:06:27 +0000

    Inject now through asterisk operator
    FossilOrigin-Name: f6820f824853a078d2659e9ffc536a51105515eeb842cf989c2802fb3e3311e1

--- t.swift
+++ t.swift
@@ -34,7 +34,13 @@ struct FileSystem {
 struct Environment {
     let fs: FileSystem
     let put: (String) -> Void
-    let date: () -> String
+    var now: () -> Date
+    
+    var date: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMddHHmmss"
+        return formatter.string(from: now())
+    }
 }
 
 // ==========================================
@@ -47,13 +53,13 @@ let make: (TodoPath, DonePath, Environment) -> t_cli = { todoPath, donePath, env
         let command = try parseArgs(args)
         switch command {
             case .list:
-                try runList(todoPath, env)
+            try runList(todoPath, env)
             case let .add(todo):
-                try runAdd(todoPath, todo, env)
+            try runAdd(todoPath, todo, env)
             case let .remove(line):
-                try runRemove(todoPath, line, env)
+            try runRemove(todoPath, line, env)
             case let .complete(line):
-                try runComplete(todoPath, donePath, line, env)
+            try runComplete(todoPath, donePath, line, env)
         }
     }
 }
@@ -61,8 +67,8 @@ let make: (TodoPath, DonePath, Environment) -> t_cli = { todoPath, donePath, env
 let runList: (TodoPath, Environment) throws(AppError) -> Void = { path, env  in
     let lines = try env.fs.read(path)
     lines.enumerated()
-        .map { idx, content in (idx + 1).description + " " + content }
-        .forEach(env.put)
+    .map { idx, content in (idx + 1).description + " " + content }
+    .forEach(env.put)
 }
 
 let runAdd: (TodoPath, String, Environment) throws(AppError) -> Void = { path, todo, env in
@@ -88,7 +94,7 @@ let runComplete: (TodoPath, DonePath, Int, Environment) throws(AppError) -> Void
     }
     try env.fs.write(rest, todoPath)
     let done = try env.fs.read(donePath)
-    try env.fs.write(done + [env.date() + " " + removed], donePath)
+    try env.fs.write(done + [env.date + " " + removed], donePath)
     env.put("Task completed")
 }
 
@@ -106,25 +112,25 @@ let parseArgs: (Args) throws(AppError) -> Command = { args throws(AppError) in
     guard let first = args.first else { return .list }
     
     switch first {
-    case "list":
+        case "list":
         guard args.count == 1 else { throw AppError.conflictingFlags }
         return .list
         
-    case "add":
+        case "add":
         guard args.count == 2 else { throw AppError.conflictingFlags }
         return .add(args[1])
         
-    case "remove":
+        case "remove":
         guard args.count == 2, let line = Int(args[1])
         else { throw AppError.conflictingFlags }
         return .remove(line)
         
-    case "complete":
+        case "complete":
         guard args.count == 2, let line = Int(args[1])
         else { throw AppError.conflictingFlags }
         return .complete(line)
         
-    default:
+        default:
         throw AppError.unhandledFlag
     }
 }
@@ -148,11 +154,7 @@ let liveEnv = Environment(
         }
     ),
     put: { text in print(text) },
-    date: { 
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyyMMddHHmmss"
-        return formatter.string(from: Date())
-    }
+    now: { Date() }
 )
 
 #if DEBUG
@@ -166,7 +168,7 @@ typealias SUT = (
     tearDown: () -> Void
 )
 
-let makeSUT: () -> SUT = {
+let makeSUT: (@escaping () -> Date) -> SUT = { now in
     let tempDir = NSTemporaryDirectory()
     let uuid = UUID().uuidString
     let todo = tempDir + "todo_\(uuid).txt"
@@ -180,7 +182,7 @@ let makeSUT: () -> SUT = {
         try? FileManager.default.removeItem(atPath: done)
     }
     
-    let t = make(todo, done, liveEnv)
+    let t = make(todo, done, liveEnv * { $0.now = now })
     
     return (t, todo, done, tearDown)
 }
@@ -214,7 +216,9 @@ let add: (SUT, String, Int) -> Void = { sut, todo, expectedIdx in
 }
 
 let integrationTest: () = {
-    let sut = makeSUT()
+    
+    let now = Calendar.current.date(from: DateComponents(year: 2016, month: 1, day: 1))!
+    let sut = makeSUT({ now })
     
     // 1. Añadir tareas
     do {
@@ -248,7 +252,7 @@ let integrationTest: () = {
     do {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyyMMddHHmmss"
-        let expectedDatePrefix = formatter.string(from: Date())
+        let expectedDatePrefix = formatter.string(from: now)
         
         let output = try! captureOutput {
             try sut.execute(["complete", "1"])
@@ -275,6 +279,16 @@ let integrationTest: () = {
     sut.tearDown()
 }()
 
+
+
+// Helpers
+infix operator *: MultiplicationPrecedence
+func *<A>(lhs: A, rhs: (inout A) -> Void) -> A {
+    var copy = lhs
+    rhs(&copy)
+    return copy
+}
+
 #endif
 
 // ==========================================
@@ -291,3 +305,5 @@ do {
 } catch {
     print("error: \(error)")
 }
+
+
