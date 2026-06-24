76665561d3276aa1441cb763cc1a600ccd6b001e
cristian <cristian.rojas@live.fr>
2026-06-24 21:59:56 +0000

    Run list returns list on succesful read
    FossilOrigin-Name: e8c87d0aca61a86af244fa5170822d0ba4ff93470c691ad106d5da7143e48cac

--- test/test_t.ml
+++ test/test_t.ml
@@ -4,7 +4,11 @@ type effects = {
 	read: string -> (string list, error) result;
 }
 
-let run_list todo_path fx = fx.read todo_path
+let ( let* ) = Result.bind
+
+let run_list todo_path fx =
+	let* lines = fx.read todo_path in
+	Ok lines
 
 (* run list returns error on read error *)
 let () =
@@ -12,3 +16,10 @@ let () =
 	match run_list "any path" fx with
 	| Error FileSystem -> ()
 	| _ -> assert false
+
+(* run list returns list on successful read *)
+let () =
+	let fx = { read = fun path -> Ok ["todo 1"] } in
+	match run_list "any path" fx with
+	| Error FileSystem -> assert false
+	| Ok list -> assert (list == ["todo 1"])
