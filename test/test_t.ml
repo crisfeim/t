98e5fa968f0058eddca07b4e9745f8912a41aa21
cristian <cristian.rojas@live.fr>
2026-06-25 20:06:16 +0000

    Run remove succeeds on successful write
    FossilOrigin-Name: 00237f3dab7b931ef97e0f21a1a9d8ee2dd2ba2e2be705384e3b78fa806d44e7

--- test/test_t.ml
+++ test/test_t.ml
@@ -54,15 +54,16 @@ let () =
 let remove line todo_path effects =
 	let* todos = effects.read todo_path in
 	if line < 1 || line > List.length todos then Error (WrongLine line) else
-	let* _ = effects.write [] todo_path in
-	Ok()
-
+  let updated = todos |> List.filteri (fun idx _ -> idx <> line - 1) in
+	let* _ = effects.write updated todo_path in
+	Ok updated
 
 let () =
 	[
 		(Error FileSystem  , 1 , Ok() 					 , Error FileSystem   );
 		(Ok ["any todo"]   , 2 , Ok()						 , Error (WrongLine 2));
-		(Ok ["any todo"]   , 1,  Error FileSystem, Error FileSystem		)
+		(Ok ["any todo"]   , 1 , Error FileSystem, Error FileSystem		);
+		(Ok ["any todo"]   , 1 , Ok()            , Ok[] 							)
 	] |> List.iter (fun (read, line, write, expected) ->
 		assert (remove line "todo path" {
 			read = (fun _ -> read) ;
