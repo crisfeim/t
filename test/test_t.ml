8f894c39d7c6d6e1e7d28816a70f872cd443be64
cristian <cristian.rojas@live.fr>
2026-06-25 19:19:35 +0000

    Use Table-Driven Testing for control flow tests
    FossilOrigin-Name: 43fc157eff0103f57897c2391b4425b65fd3725f4b8b52a1e3298154d1d52d02

--- test/test_t.ml
+++ test/test_t.ml
@@ -14,61 +14,36 @@ let run_list todo_path fx =
 	let formatted = todos |> List.mapi (fun idx content -> string_of_int (idx + 1) ^ " " ^ content) in
 	Ok formatted
 
-(* Run list fails on read error *)
-let () =
-	let fx = { read = (fun _ -> Error FileSystem) ; write = (fun _ _ -> Ok()) } in
-	match run_list "any path" fx with
-	| Error FileSystem -> ()
-	| _ -> assert false
-
-(* Run list succeeds on successful read *)
-let () =
-	let fx = { read = (fun _ -> Ok ["todo 1"]) ; write = (fun _ _ -> Ok()) } in
-	match run_list "any path" fx with
-	| Error FileSystem -> assert false
-	| Ok list -> assert (list = ["1 todo 1"])
-
-
 let run_add todo todo_path fx =
 	let* todos = fx.read todo_path in
 	let updated = todos @ [todo] in
 	let* _ = fx.write updated todo_path in
 	Ok()
 
-(* Run add fails on read failure *)
+(* Run list test case *)
 let () =
-		let fx = { read = (fun _ -> Error FileSystem) ; write = (fun _ _ -> Ok() ) } in
-		match run_add "any todo" "any path" fx with
-		| Error FileSystem -> ()
-		| _ -> assert false
-
-(* Run add fails on writing failure *)
-let () =
-	let fx = { read = (fun _ -> Ok []) ; write = (fun _ _ -> Error FileSystem) } in
-	match run_add "any todo" "any path" fx with
-	| Error FileSystem -> ()
-	| _ -> assert false
-
-
-let run_list_matrix = [
-  (* Caso 1: Error en la lectura -> Debe propagar el error *)
-  (Error FileSystem, Error FileSystem);
-  (* Caso 2: Lectura exitosa vacía -> Lista vacía *)
-  (Ok [], Ok []);
-  (* Caso 3: Lectura exitosa con datos -> Lista formateada *)
-  (Ok ["compra"; "lavar"], Ok ["1 compra"; "2 lavar"])
-]
-
-(* 3. El Ejecutor de la Matriz *)
-let () =
-  run_list_matrix |> List.iter (fun (effect_behaviour, expected_result) ->
-    (* Configurar el SUT (System Under Test) dinámicamente basado en la fila *)
-    let fx = {
-      read =  (fun _ -> effect_behaviour);
-      write = (fun _ _ -> Ok ());
-    } in
-
-    (* Ejecutar y asegurar la coincidencia absoluta *)
-    let actual_result = run_list "any_path" fx in
-    assert (actual_result = expected_result)
+	[
+	  (Error FileSystem			 , Error FileSystem					);
+	  (Ok ["compra"; "lavar"], Ok ["1 compra"; "2 lavar"])
+	]
+	|> List.iter (fun (read, expected) ->
+			assert (run_list "any todo path" {
+     		read  = (fun _ -> read);
+       	write = (fun _ _ -> Ok ());
+   			} = expected
+			)
   )
+
+(* Run add *)
+let () =
+	[
+		(* read *)       (* write *)       (* expected *)
+		(Error FileSystem, Ok()            , Error FileSystem);
+		(Ok[]            , Error FileSystem, Error FileSystem);
+		(Ok[]						 , Ok()					   , Ok()            )
+	] |> List.iter (fun (read, write, expected) ->
+		assert (run_add "any todo path" "any done path" {
+			read = (fun _ -> read) ;
+			write = (fun _ _ -> write)
+		} = expected)
+	)
