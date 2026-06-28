aee8245286d7310029b750a06d3556e53fec2d30
cristian <cristian.rojas@live.fr>
2026-06-28 12:25:04 +0000

    Make mini test lib exist for CI
    FossilOrigin-Name: 70e0e31c30f379e7f3560d182e063df20b1d3a637376d007b57f7466c6948b28

--- test/test.ml
+++ test/test.ml
@@ -6,17 +6,13 @@ type expect = {
   equal : 'a. 'a -> 'a -> unit;
   fail  : string -> unit;
 }
-
 let format_error description message =
   if message = "" then Printf.sprintf "\u{001B}[91m􀢄\u{001B}[0m  %s" description
   else Printf.sprintf "\u{001B}[91m􀢄\u{001B}[0m  %s: %s" description message
-
 exception Test_failed
-
 let raise_error description message errors_ref =
   errors_ref := format_error description message :: !errors_ref;
   raise Test_failed
-
 let make_expect description errors_ref =
   let is bool message = if bool then () else raise_error description message errors_ref in
   { is;
@@ -24,6 +20,8 @@ let make_expect description errors_ref =
       if expected = actual then ()
       else raise_error description "" errors_ref);
     fail = (fun message -> raise_error description message errors_ref) }
+
+let exit_code = ref 0
 
 let case name fn =
   let passed = ref 0 in
@@ -37,7 +35,10 @@ let case name fn =
         errors := format_error description ("Failed: " ^ Printexc.to_string exn) :: !errors;
         failed := !failed + 1;
   );
+  if !failed > 0 then exit_code := 1;
   Printf.printf "\u{001B}[1m%s\u{001B}[0m: \u{001B}[92mPassed: %d\u{001B}[0m | \u{001B}[91mFailed: %d\u{001B}[0m\n"
     name !passed !failed;
   List.iter print_endline (List.rev !errors);
   print_newline ()
+
+let () = at_exit (fun () -> exit !exit_code)
