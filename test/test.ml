29ed723c9fc04b62c70597395629ab8285a75920
cristian <cristian.rojas@live.fr>
2026-07-01 02:01:41 +0000

    Align columns on test output
    FossilOrigin-Name: 4125988a70fc5e87b67460f67c9421107eb057f0bc689ee74f72f4b7c3434170

--- test/test.ml
+++ test/test.ml
@@ -9,10 +9,13 @@ type expect = {
 let format_error description message =
   if message = "" then Printf.sprintf "\u{001B}[91m􀢄\u{001B}[0m  %s" description
   else Printf.sprintf "\u{001B}[91m􀢄\u{001B}[0m  %s: %s" description message
+
 exception Test_failed
+
 let raise_error description message errors_ref =
   errors_ref := format_error description message :: !errors_ref;
   raise Test_failed
+
 let make_expect description errors_ref =
   let is bool message = if bool then () else raise_error description message errors_ref in
   { is;
@@ -36,8 +39,8 @@ let case name fn =
         failed := !failed + 1;
   );
   if !failed > 0 then exit_code := 1;
-  Printf.printf "\u{001B}[1m%s\u{001B}[0m: \u{001B}[92mPassed: %d\u{001B}[0m | \u{001B}[91mFailed: %d\u{001B}[0m"
-    name !passed !failed;
+
+  Printf.printf "\u{001B}[1m%-20s\u{001B}[0m | \u{001B}[92mPassed: %2d\u{001B}[0m | \u{001B}[91mFailed: %2d\u{001B}[0m" name !passed !failed;
   if !errors <> [] then begin
     print_newline ();
     List.iter print_endline (List.rev !errors);
