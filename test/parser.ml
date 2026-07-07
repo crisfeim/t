adaec7ae0ef9c32f5e7828e4822c66f57d3f1b85
cristian <cristian.rojas@live.fr>
2026-07-07 19:51:53 +0000

    Add editing case parsing
    FossilOrigin-Name: 9842a2551ad94455bd14bc18b89e865b7bb00d7e3e380f8cd9874ad0188d7065

--- test/parser.ml
+++ test/parser.ml
@@ -6,6 +6,7 @@ type command =
 | Add of string
 | Complete of int
 | Remove of int
+| Edit of int
 
 let get_line string = int_of_string (String.sub string 1 (String.length string - 1))
 
@@ -19,10 +20,16 @@ let is_remove_cmd str =
   && String.get str 0 = '-'
   && Option.is_some (int_of_string_opt (String.sub str 1 (String.length str - 1)))
 
+let is_edit_cmd str =
+  String.length str > 1
+  && String.get str 0 = '~'
+  && Option.is_some (int_of_string_opt (String.sub str 1 (String.length str - 1)))
+
 let parser todo_path args = match args with
 	| [] -> Some (List todo_path)
 	| [single] when is_complete_cmd single -> Some (Complete (get_line single))
 	| [single] when is_remove_cmd single -> Some (Remove (get_line single))
+	| [single] when is_edit_cmd single -> Some (Edit (get_line single))
 	| values -> Some (Add (String.concat " " values))
 
 let () = case "Parser" (fun test ->
@@ -44,5 +51,10 @@ let () = case "Parser" (fun test ->
 	test "Remove" (fun expect ->
 		let command = parser "any path" ["-32"] in
 		expect.equal command (Some (Remove 32))
+	);
+
+	test "Edit" (fun expect ->
+		let command = parser "any path" ["~32"] in
+		expect.equal command (Some (Edit 32))
 	);
 )
