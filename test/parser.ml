8f0b825850fe2959405bce801bf9ede97d5d8070
cristian <cristian.rojas@live.fr>
2026-07-07 19:25:36 +0000

    Parser: list case
    FossilOrigin-Name: b284b967c8ec054a600e720e1ebee840c29b6da41a892c4ef2262b40b6fef21b

--- test/parser.ml
+++ test/parser.ml
@@ -1,13 +1,22 @@
 open Test_lib
+open T
 
 type command =
+| List of path
 | Add of string
 
-let parser args = Add args
+let parser todo_path args = match args with
+	| [] -> List todo_path
+	| values -> Add (String.concat " " values)
 
 let () = case "Parser" (fun test ->
+	test "List" (fun expect ->
+		let command = parser  "any path" [] in
+		expect.equal command (List "any path")
+	);
+
 	test "Add" (fun expect ->
-		let command = parser "new todo" in
+		let command = parser "any path" ["new";"todo"] in
 		expect.equal command (Add "new todo")
 	)
 )
