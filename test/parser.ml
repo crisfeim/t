a7d4cf17e73e4e0f9a2e450e1824139a7f36e4f7
cristian <cristian.rojas@live.fr>
2026-07-07 21:39:18 +0000

    Add parsing of multiple completed lines
    FossilOrigin-Name: ac78f1e9f2b919ed5426739fca3c3fa3d989e2e0e69339c45a230cb76418eaf6

--- test/parser.ml
+++ test/parser.ml
@@ -4,7 +4,7 @@ open T
 type command =
 | List of path
 | Add of string
-| Complete of int
+| Complete of int list
 | Remove of int
 | Edit of int
 | Commit of int * bool
@@ -16,19 +16,34 @@ let cmd operator str =
   && String.get str 0 = operator
   && Option.is_some (int_of_string_opt (String.sub str 1 (String.length str - 1)))
 
+let drop n str =
+	if n >=String.length str then ""
+	else String.sub str n (String.length str - n)
+
+let is_numeric str = Option.is_some (int_of_string_opt str)
+
+let is_complete_cmd str =
+	String.length str > 1
+  && String.get str 0 = '+'
+	&& (drop 1 str
+		|> String.split_on_char ','
+		|> List.for_all is_numeric)
+
 let commit_editing str =
 	String.length str > 2
 	&& String.get str 0 = 'c'
 	&& String.get str 1 = '~'
 	&& Option.is_some (int_of_string_opt (String.sub str 2 (String.length str - 2)))
 
+let list_from string = String.split_on_char ',' string
+
 let parser todo_path args = match args with
 	| [] -> Some (List todo_path)
-	| [single] when cmd '+' single -> Some (Complete (get_line single 1))
-	| [single] when cmd '-' single -> Some (Remove (get_line single 1))
-	| [single] when cmd '~' single -> Some (Edit (get_line single 1))
-	| [single] when cmd 'c' single -> Some (Commit ((get_line single 1), false))
-	| [single] when commit_editing single -> Some (Commit ((get_line single 2), true))
+	| [single] when is_complete_cmd single -> Some (Complete ((list_from (drop 1 single)) |> List.map int_of_string))
+	| [single] when cmd '-' single -> Some (Remove (int_of_string (drop 1 single)))
+	| [single] when cmd '~' single -> Some (Edit (int_of_string (drop 1 single)))
+	| [single] when cmd 'c' single -> Some (Commit (int_of_string (drop 1 single), false))
+	| [single] when commit_editing single -> Some (Commit (int_of_string (drop 2 single), true))
 	| values -> Some (Add (String.concat " " values))
 
 let () = case "Parser" (fun test ->
@@ -42,9 +57,14 @@ let () = case "Parser" (fun test ->
 		expect.equal command (Some (Add "new todo"))
 	);
 
-	test "Complete" (fun expect ->
+	test "Complete one" (fun expect ->
 		let command = parser "any path" ["+32"] in
-		expect.equal command (Some (Complete 32))
+		expect.equal command (Some (Complete [32]))
+	);
+
+	test "Complete many" (fun expect ->
+		let command = parser "any path" ["+32,24"] in
+		expect.equal command (Some (Complete [32;24]))
 	);
 
 	test "Remove" (fun expect ->
