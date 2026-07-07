d247cd1b8bf85417c87b1b6e9257474c01ebda5c
cristian <cristian.rojas@live.fr>
2026-07-07 19:48:40 +0000

    Add remove case
    FossilOrigin-Name: 947514cb8d5396b87f4178f2d686e55ca974c44fc8aba8c4f2b3abe7ac18e106

--- test/parser.ml
+++ test/parser.ml
@@ -5,6 +5,7 @@ type command =
 | List of path
 | Add of string
 | Complete of int
+| Remove of int
 
 let get_line string = int_of_string (String.sub string 1 (String.length string - 1))
 
@@ -13,9 +14,15 @@ let is_complete_cmd str =
   && String.get str 0 = '+'
   && Option.is_some (int_of_string_opt (String.sub str 1 (String.length str - 1)))
 
+let is_remove_cmd str =
+  String.length str > 1
+  && String.get str 0 = '-'
+  && Option.is_some (int_of_string_opt (String.sub str 1 (String.length str - 1)))
+
 let parser todo_path args = match args with
 	| [] -> Some (List todo_path)
 	| [single] when is_complete_cmd single -> Some (Complete (get_line single))
+	| [single] when is_remove_cmd single -> Some (Remove (get_line single))
 	| values -> Some (Add (String.concat " " values))
 
 let () = case "Parser" (fun test ->
@@ -32,5 +39,10 @@ let () = case "Parser" (fun test ->
 	test "Complete" (fun expect ->
 		let command = parser "any path" ["+32"] in
 		expect.equal command (Some (Complete 32))
-	)
+	);
+
+	test "Remove" (fun expect ->
+		let command = parser "any path" ["-32"] in
+		expect.equal command (Some (Remove 32))
+	);
 )
