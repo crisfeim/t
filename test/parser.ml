4a01e173b393b05ac2ae5d0f780da86b868f92e8
cristian <cristian.rojas@live.fr>
2026-07-07 21:13:11 +0000

    Add commit editing case to parser
    FossilOrigin-Name: 2d0479ccad97678724e10b8731295526392447033a9ceeb52c7666d02c0bc31c

--- test/parser.ml
+++ test/parser.ml
@@ -7,21 +7,28 @@ type command =
 | Complete of int
 | Remove of int
 | Edit of int
-| Commit of int
+| Commit of int * bool
 
-let get_line string = int_of_string (String.sub string 1 (String.length string - 1))
+let get_line string from = int_of_string (String.sub string from (String.length string - from))
 
 let cmd operator str =
 	String.length str > 1
   && String.get str 0 = operator
   && Option.is_some (int_of_string_opt (String.sub str 1 (String.length str - 1)))
 
+let commit_editing str =
+	String.length str > 2
+	&& String.get str 0 = 'c'
+	&& String.get str 1 = '~'
+	&& Option.is_some (int_of_string_opt (String.sub str 2 (String.length str - 2)))
+
 let parser todo_path args = match args with
 	| [] -> Some (List todo_path)
-	| [single] when cmd '+' single -> Some (Complete (get_line single))
-	| [single] when cmd '-' single -> Some (Remove (get_line single))
-	| [single] when cmd '~' single -> Some (Edit (get_line single))
-	| [single] when cmd 'c' single -> Some (Commit (get_line single))
+	| [single] when cmd '+' single -> Some (Complete (get_line single 1))
+	| [single] when cmd '-' single -> Some (Remove (get_line single 1))
+	| [single] when cmd '~' single -> Some (Edit (get_line single 1))
+	| [single] when cmd 'c' single -> Some (Commit ((get_line single 1), false))
+	| [single] when commit_editing single -> Some (Commit ((get_line single 2), true))
 	| values -> Some (Add (String.concat " " values))
 
 let () = case "Parser" (fun test ->
@@ -52,6 +59,11 @@ let () = case "Parser" (fun test ->
 
 	test "Commit" (fun expect ->
 		let command = parser "any path" ["c32"] in
-		expect.equal command (Some (Commit 32))
+		expect.equal command (Some (Commit (32, false)))
+	);
+
+	test "Commit editing" (fun expect ->
+		let command = parser "any path" ["c~32"] in
+		expect.equal command (Some (Commit (32, true)))
 	);
 )
