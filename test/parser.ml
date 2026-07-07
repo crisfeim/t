8642f35f498a05fe4b0d15e1cc98861e38551ac6
cristian <cristian.rojas@live.fr>
2026-07-07 22:52:47 +0000

    Parser: t @2  (Set todo at line 3 as @doing)
    FossilOrigin-Name: dc2e054ee6aa470f361d76e506539e7aa1ff1e5d8b9a308521bfa67402e6a793

--- test/parser.ml
+++ test/parser.ml
@@ -10,6 +10,7 @@ type command =
 | Commit of int * bool
 | Echo of int
 | EditFile
+| Doing of int
 
 let get_line string from = int_of_string (String.sub string from (String.length string - from))
 
@@ -45,6 +46,7 @@ let parser args = match args with
 	| [single] when batch_cmd '-' single -> Some (Remove ((list_from (drop 1 single)) |> List.map int_of_string))
 	| [single] when cmd ':' single -> Some (Edit (int_of_string (drop 1 single)))
 	| [single] when cmd 'c' single -> Some (Commit (int_of_string (drop 1 single), false))
+	| [single] when cmd '@' single -> Some (Doing (int_of_string (drop 1 single)))
 	| [single] when cmd_c_editing single -> Some (Commit (int_of_string (drop 2 single), true))
 	| [single] when Option.is_some (int_of_string_opt single) -> Some (Echo (int_of_string single))
 	| [single] when single = ":" -> Some (EditFile)
@@ -104,5 +106,10 @@ let () = case "Parser" (fun test ->
 	test "Commit editing" (fun expect ->
 		let command = parser ["c:32"] in
 		expect.equal command (Some (Commit (32, true)))
+	);
+
+	test "Mark as doing" (fun expect ->
+		let command = parser ["@32"] in
+		expect.equal command (Some (Doing 32))
 	);
 )
