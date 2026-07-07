32e9ce821b3332cd163bf87a1cd651d5ac59873f
cristian <cristian.rojas@live.fr>
2026-07-07 19:41:11 +0000

    Parser: complete
    FossilOrigin-Name: 2f847bcee0a34afb3522b46e8eaf44eb7612118bb6b6c8dc2f62469734598aa7

--- test/parser.ml
+++ test/parser.ml
@@ -4,19 +4,37 @@ open T
 type command =
 | List of path
 | Add of string
+| Complete of int
+
+let get_substring string from = String.sub string from (String.length string - from)
+
+let parse_compact cmd =
+  if String.length cmd > 1 && String.get cmd 0 = '+' then
+    let num_str = get_substring cmd 1 in
+    match int_of_string_opt num_str with
+    | Some line -> Some (Complete line)
+    | None -> None
+  else
+    None
 
 let parser todo_path args = match args with
-	| [] -> List todo_path
-	| values -> Add (String.concat " " values)
+	| [] -> Some (List todo_path)
+	| [single_arg] -> parse_compact single_arg
+	| values -> Some (Add (String.concat " " values))
 
 let () = case "Parser" (fun test ->
 	test "List" (fun expect ->
 		let command = parser  "any path" [] in
-		expect.equal command (List "any path")
+		expect.equal command (Some (List "any path"))
 	);
 
 	test "Add" (fun expect ->
 		let command = parser "any path" ["new";"todo"] in
-		expect.equal command (Add "new todo")
+		expect.equal command (Some (Add "new todo"))
+	);
+
+	test "Complete" (fun expect ->
+		let command = parser "any path" ["+32"] in
+		expect.equal command (Some (Complete 32))
 	)
 )
