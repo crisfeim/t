a058a8d89df0278b0c0144034b668782ac66df3b
cristian <cristian.rojas@live.fr>
2026-07-11 16:29:28 +0000

    Remove in prepration to merge
    FossilOrigin-Name: c329d7bb6a856f6a7b729b66af2cb0be887afd8b8fbefe157cc17628c046f767

--- test.swift
+++
@@ -1,8 +0,0 @@
-func assertThrows<E: Error & Equatable>(_ expected: E, line: UInt = #line, _ block: () throws(E) -> Void) {
-    do throws(E) {
-        try block()
-        assert(false, line: line)
-    } catch {
-        assert(error == expected, line: line)
-    }
-}
