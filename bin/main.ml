c7fb71c6b34474b551e87bd24e5b5370872126bd
cristian <cristian.rojas@live.fr>
2026-07-11 13:18:11 +0000

    Add dispatcher to avoid nested pattern matching
    FossilOrigin-Name: 9ae4ea7b9574adb1f08e8e5ec926a4bde80f3b5beab2ef21669eaffe1a7f1b1b

--- bin/main.ml
+++ bin/main.ml
@@ -46,6 +46,44 @@ let get_done_path () =
   let current_dir = Sys.getcwd () in
   Filename.concat current_dir ".done"
 
+(* dispatch cmd -> result *)
+let dispatch cmd todo_path done_path effects = match cmd with
+	| Count ->
+		let* todos = list todo_path effects in
+		print_endline (string_of_int (List.length todos));
+		Ok()
+	| List path ->
+		let* todos = list path effects in
+		List.iter (fun todo -> print_endline todo) todos;
+		Ok()
+	| ListRange (path, range) ->
+		let* todos = list path effects in
+		todos
+			|> List.filteri (fun i _ -> List.mem (i + 1) range)
+			|> List.iter (fun todo -> print_endline todo);
+		Ok()
+	| Add (path, todo) ->
+		let* added = add todo path effects in
+		print_endline added;
+		Ok()
+	| Complete (path, lines) ->
+			let* completed = complete (List.hd lines) path done_path effects in
+			print_endline completed;
+			Ok()
+	| Remove (path, lines) ->
+		let* removed = remove (List.hd lines) todo_path effects in
+		print_endline removed;
+		Ok()
+	| Edit (path, line) -> print_endline "@todo: edit" ; Ok()
+  | Commit (path, line, msg) -> print_endline "@todo: commit" ; Ok()
+  | Echo (path, line) -> print_endline "@todo: echo"  ; Ok()
+  | EditFile path -> print_endline "@todo: edit file"  ; Ok()
+  | Doing (path, line) -> print_endline "@todo: doing"  ; Ok()
+  | ListDoing path -> print_endline "@todo: list doing"  ; Ok()
+  | ListProjects -> print_endline "@todo: list projects"  ; Ok()
+  | ListDoingAcrossProjects -> print_endline "@todo: list doing across projects" ; Ok()
+
+
 let () =
   let args = Array.to_list Sys.argv in
   let cli_args = match args with [] -> [] | _::tl -> tl in
@@ -54,39 +92,8 @@ let () =
   let done_path = get_done_path() in
   match command_router todo_path cli_args effects with
   | Some cmd ->
-      begin match cmd with
-      | Count -> begin match (list todo_path effects) with
-      	| Ok todos -> print_endline (string_of_int (List.length todos))
-        | Error _ -> print_endline "Error"
-        end
-      | List path -> begin match (list path effects) with
-      	| Ok todos -> List.iter (fun todo -> print_endline todo) todos
-      	| Error _ -> print_endline "Error"
-       	end
-      | ListRange (path, range) -> begin match (list path effects) with
-      	|	Ok todos -> todos |> List.filteri (fun i _ -> List.mem (i + 1) range)
-       		|> List.iter (fun todo -> print_endline todo)
-       	| Error _ -> print_endline "Error"
-      	end
-      | Add (path, todo) -> begin match (add todo path effects) with
-      	| Ok todo -> print_endline todo
-        | Error _ -> print_endline "Error"
-      	end
-      | Complete (path, lines) -> begin match (complete (List.hd lines) path done_path effects) with
-      	| Ok todo -> print_endline todo
-       	| Error _ -> print_endline "Error"
-       end
-      | Remove (path, lines) -> begin match (remove (List.hd lines) path effects) with
-      	| Ok todo -> print_endline todo
-        | Error _ -> print_endline "Error"
-        end
-      | Edit (path, line) -> print_endline "@todo: edit"
-      | Commit (path, line, msg) -> print_endline "@todo: commit"
-      | Echo (path, line) -> print_endline "@todo: echo"
-      | EditFile path -> print_endline "@todo: edit file"
-      | Doing (path, line) -> print_endline "@todo: doing"
-      | ListDoing path -> print_endline "@todo: list doing"
-      | ListProjects -> print_endline "@todo: list projects"
-      | ListDoingAcrossProjects -> print_endline "@todo: list doing across projects"
+  		begin match (dispatch cmd todo_path done_path effects) with
+    	| Ok () -> ()
+     	| Error _ -> print_endline "error"
       end
   | None -> print_endline "@todo: none"
