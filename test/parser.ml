ae61de3f38b4eef0dcf6abd5300f410f2ff20ffa
cristian <cristian.rojas@live.fr>
2026-07-11 23:58:37 +0000

    Invert arguments passed to expect.equal in parser so it complies with api (Expected <something> got <something else>)
    FossilOrigin-Name: 9e999152ac816e479f173e6ca7ccf3f368ef28da1342814d1dcb9979286003c2

--- test/parser.ml
+++ test/parser.ml
@@ -7,79 +7,79 @@ let any_todo_path = "any-todo-path"
 let () = case "Parser" (fun test ->
 
 	test "count" (fun expect ->
-		expect.equal (fmt_command) (Count) (parser any_todo_path ["count"])
+		expect.equal (fmt_command) Count (parser any_todo_path ["count"])
 	);
 
 	test "Echo" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["10"]) (Echo (any_todo_path, 10))
+		expect.equal (fmt_command) (Echo (any_todo_path, 10)) (parser any_todo_path ["10"])
 	);
 
 	test "List" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path []) (List any_todo_path)
+		expect.equal (fmt_command) (List any_todo_path) (parser any_todo_path [])
 	);
 
 	test "List projects" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["."]) ListProjects
+		expect.equal (fmt_command) ListProjects (parser any_todo_path ["."])
 	);
 
 	test "List doing across projects" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path [".@"]) ListDoingAcrossProjects
+		expect.equal (fmt_command) ListDoingAcrossProjects (parser any_todo_path [".@"])
 	);
 
 	test "List range" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["1...5"]) (ListRange (any_todo_path, [1;2;3;4;5]))
+		expect.equal (fmt_command) (ListRange (any_todo_path, [1;2;3;4;5])) (parser any_todo_path ["1...5"])
 	);
 
 	test "Parse range" (fun expect ->
-		expect.equal fmt_int_list (parse_range "1...3") [1;2;3]
+		expect.equal fmt_int_list [1;2;3] (parse_range "1...3")
 	);
 
 	test "List doing" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["@"]) (ListDoing any_todo_path)
+		expect.equal (fmt_command) (ListDoing any_todo_path) (parser any_todo_path ["@"])
 	);
 
 	test "Add" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["new";"todo"]) (Add (any_todo_path, "new todo"))
+		expect.equal (fmt_command) (Add (any_todo_path, "new todo")) (parser any_todo_path ["new";"todo"])
 	);
 
 	test "Complete one" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["+32"]) (Complete (any_todo_path, [32]))
+		expect.equal (fmt_command) (Complete (any_todo_path, [32])) (parser any_todo_path ["+32"])
 	);
 
 	test "Complete many" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["+32,24"]) (Complete (any_todo_path, [32;24]))
+		expect.equal (fmt_command) (Complete (any_todo_path, [32;24])) (parser any_todo_path ["+32,24"])
 	);
 
 	test "Remove" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["-32"]) (Remove (any_todo_path, [32]))
+		expect.equal (fmt_command) (Remove (any_todo_path, [32])) (parser any_todo_path ["-32"])
 	);
 
 	test "Remove many" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["-32,24"]) (Remove (any_todo_path, [32;24]))
+		expect.equal (fmt_command) (Remove (any_todo_path, [32;24])) (parser any_todo_path ["-32,24"])
 	);
 
 	test "Edit" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path [":32"]) (Edit (any_todo_path, 32))
+		expect.equal (fmt_command) (Edit (any_todo_path, 32)) (parser any_todo_path [":32"])
 	);
 
 	test "Update" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path [":32"; "value"]) (Update (any_todo_path, 32, "value"))
+		expect.equal (fmt_command) (Update (any_todo_path, 32, "value")) (parser any_todo_path [":32"; "value"])
 	);
 
 	test "Edit .todo" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path [":"]) (EditFile any_todo_path)
+		expect.equal (fmt_command) (EditFile any_todo_path) (parser any_todo_path [":"])
 	);
 
 	test "Commit" (fun expect ->
-		expect.equal (fmt_command) (parser  any_todo_path ["c32"]) (Commit (any_todo_path, 32, false))
+		expect.equal (fmt_command) (Commit (any_todo_path, 32, false)) (parser  any_todo_path ["c32"])
 	);
 
 	test "Commit editing" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["c:32"]) (Commit (any_todo_path, 32, true))
+		expect.equal (fmt_command) (Commit (any_todo_path, 32, true)) (parser any_todo_path ["c:32"])
 	);
 
 	test "Mark as doing" (fun expect ->
-		expect.equal (fmt_command) (parser any_todo_path ["@32"]) (Doing (any_todo_path, 32))
+		expect.equal (fmt_command) (Doing (any_todo_path, 32)) (parser any_todo_path ["@32"])
 	);
 )
 
